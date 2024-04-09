dd197a430f56e642011e324ccaa9a8c5267ff4c9
Abhirup <abhirupkumar001@gmail.com>
2024-04-09 20:28:28 +0530

    Updated The Admin Menu

--- src/Authentication/SignUp.java
+++ src/Authentication/SignUp.java
@@ -23,7 +23,7 @@ public class SignUp {
   }
 
   public void createCustomersTableIfNotExists() throws SQLException {
-    String sql = "CREATE TABLE IF NOT EXISTS customers (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50), password VARCHAR(50))";
+    String sql = "CREATE TABLE IF NOT EXISTS customers (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50), password VARCHAR(50), name VARCHAR(20))";
     PreparedStatement stmt = con.prepareStatement(sql);
     stmt.execute();
   }
@@ -37,24 +37,22 @@ public class SignUp {
     UserName = sc.nextLine().toLowerCase().replaceAll("\\s", "");
     System.out.print("Password: ");
     Password = sc.nextLine();
-    addToSql();
+    System.out.println("Enter Name: ");
+    String Name = sc.nextLine();
+    addToSql(Name);
   }
 
-  public void addToSql() {
+  public void addToSql(String Name) {
     try {
-      String sql = "INSERT INTO customers (username, password) VALUES (?, ?)";
+      String sql = "INSERT INTO customers (username, password, Name) VALUES (?, ?, ?)";
       PreparedStatement stmt = con.prepareStatement(sql);
       stmt.setString(1, UserName);
       stmt.setString(2, Password);
+      stmt.setString(3, Name);
       stmt.executeUpdate();
       System.out.println("Successfully Signed Up.");
     } catch (SQLException e) {
       e.printStackTrace();
     }
-  }
-
-  public static void main(String[] args) {
-    SignUp signUp = new SignUp();
-    signUp.signUp(); // Call sign-up process
   }
 }
