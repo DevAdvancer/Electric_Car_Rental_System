40a31585dcd6c64fb4550d67d939bca7ba84a528
Abhirup <abhirupkumar001@gmail.com>
2024-04-11 13:53:45 +0530

    Updated the Authentication

--- src/Authentication/Login.java
+++ src/Authentication/Login.java
@@ -1,11 +1,7 @@
 package Authentication;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Scanner;
+import java.sql.*;
 
 public class Login {
   private Connection con;
